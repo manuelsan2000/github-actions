faddca2a5616d6991108ffe1d0206a1a7b28d351
Manuel Coria <manuel.coria@Manuels-MacBook-Pro.local>
2022-09-20 18:32:07 -0500

    feat: some feature

---
+++ .github/release-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/manuelsan2000/github-actions",
+    plugins: ["@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", "@semantic-release/github"]
+
+}
