77a2c8cd9bdf78c77d976ddc2f544b110db130db
Manuel Coria <manuel.coria@Manuels-MacBook-Pro.local>
2022-09-21 00:41:42 -0500

    Fix:another try

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with GHActions
+          Learn React with GHActions22
         </a>
       </header>
     </div>
