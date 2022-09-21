bf2a34a7a48625d426fb82374fd3f995bc06a4a7
Manuel Coria <manuel.coria@Manuels-MacBook-Pro.local>
2022-09-21 00:58:02 -0500

    Release:new release after all

--- src/App.js
+++ src/App.js
@@ -5,9 +5,19 @@ function nothing() {
   console.log("Here");
 }
 
+function nothing_else() {
+  console.log("Here and there");
+  let i = 1;
+  if( i === 1 ){
+    console.log("here too");
+  }else{
+    console.log("not here");
+  }
+}
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" onClick={ nothing_else }>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" onClick={ nothing }/>
         <p>
